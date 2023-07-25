bc3b9849234ff715de9507272398801755a5004e
VicGar21 <victorgarp15@hotmail.com>
2023-07-25 09:18:13 -0600

    Agrega clases a los div

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const cuadricula = document.getElementById("contenedor");
 
 for (let i = 1; i <= 16; i++) {
     const fila = document.createElement("div");
+    fila.classList.add("fila");
     fila.style.border = "solid 10px blue";
     cuadricula.appendChild(fila);
     for (let j = 1; j <= 16; j ++) {
         const columna = document.createElement("div");
+        columna.classList.add("columna")
         columna.style.border = "solid 1px red";
         cuadricula.appendChild(columna);
     }
