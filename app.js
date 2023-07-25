b9bc45ec4c51ff7783d10f191095ebc8e034bb76
VicGar21 <victorgarp15@hotmail.com>
2023-07-25 11:48:42 -0600

    Cambia de color cada cuadrado

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ for (let i = 1; i <= 256; i++) {
     const cuadrado = document.createElement("div");
     cuadrado.classList.add("cuadrado");
     cuadricula.appendChild(cuadrado);
+
+    // Agrega el evento "mouseover" a cada cuadrado individualmente
+    cuadrado.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "orange";
+    });
+
+    // Eliminar el evento anterior
+    cuadrado.addEventListener("mouseout", (e) => {
+        e.target.style.backgroundColor = "#FFFFFF";
+    })
 }
