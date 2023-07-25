af6673685d378515084119c85a278a8fdc8ebf28
VicGar21 <victorgarp15@hotmail.com>
2023-07-25 11:00:56 -0600

    Elimina estilo del div

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 // Crea la cuadricula de div
 const cuadricula = document.getElementById("contenedor");
 
-for (let i = 1; i <= 16; i++) {
-    const fila = document.createElement("div");
-    fila.classList.add("fila");
-    fila.style.border = "solid 5px blue";
-    cuadricula.appendChild(fila);
-    for (let j = 0; j < 1; j ++) {
-        const columna = document.createElement("div");
-        columna.classList.add("columna")
-        columna.style.border = "solid 5px red";
-        cuadricula.appendChild(columna);
-    }
+for (let i = 1; i <= 256; i++) {
+    const cuadrado = document.createElement("div");
+    cuadrado.classList.add("cuadrado");
+    cuadricula.appendChild(cuadrado);
 }
