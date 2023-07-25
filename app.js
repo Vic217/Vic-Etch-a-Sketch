e4b31d6b1c51e70d05f7031cb57f8bab2c03e3d0
VicGar21 <victorgarp15@hotmail.com>
2023-07-25 09:15:35 -0600

    Crea divs para cuadricula

---
+++ app.js
@@ -0,0 +1,13 @@
+// Crea la cuadricula de div
+const cuadricula = document.getElementById("contenedor");
+
+for (let i = 1; i <= 16; i++) {
+    const fila = document.createElement("div");
+    fila.style.border = "solid 10px blue";
+    cuadricula.appendChild(fila);
+    for (let j = 1; j <= 16; j ++) {
+        const columna = document.createElement("div");
+        columna.style.border = "solid 1px red";
+        cuadricula.appendChild(columna);
+    }
+}
