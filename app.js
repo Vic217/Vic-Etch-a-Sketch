c55896181be745f200ec0f06519a06bd919187af
VicGar21 <victorgarp15@hotmail.com>
2023-07-28 16:43:58 -0600

    Crea pizarra default

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ const borrador = document.getElementById("borrador");
 const size = document.getElementById("slider");
 const contenedorTam = document.getElementById("tamaño");
 
-// Crea la cuadricula de div
-const cuadricula = document.getElementById("contenedor");
-
+// Creacion de primer pizarra
 for (let i = 1; i <= 256; i++) {
     const cuadrado = document.createElement("div");
     cuadrado.classList.add("cuadrado");
-    cuadricula.appendChild(cuadrado);
+    cuadrado.style.width = (480 / 16) + "px";
+    cuadrado.style.height = (480 / 16) + "px";
+    contenedor.appendChild(cuadrado);
 
-    // Agrega el evento "mouseover" a cada cuadrado individualmente
-    cuadrado.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "orange";
+    // Seguimiento del mouse
+    cuadrado.addEventListener("mouseenter", (e) => {
+        cuadrado.classList.add("estado");
     });
 
-    // Eliminar el evento anterior
+    // Elimina el seguimiento
     cuadrado.addEventListener("mouseout", (e) => {
-        e.target.style.backgroundColor = "#FFFFFF";
+        cuadrado.classList.remove("estado");
+    });
+    
+    // Eleccion de color negro
+    activarNegro.addEventListener("click", (e) => {
+        e.target.style.backgroundColor = "#2C2B2B";
+        aleatorios.style.backgroundColor = "#FFFFFF";
+        borrador.style.backgroundColor = "#FFFFFF";
+        borrar.style.backgroundColor = "#FFFFFF";
+        cuadrado.addEventListener(accion, (e) => {
+            e.target.style.backgroundColor = "#000000";
+        });
     });
 }
